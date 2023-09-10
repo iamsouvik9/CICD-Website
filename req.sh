47fe9fb806a4de5b324c8e35a21bca952ebcdedb
SOUVIK GHOSH <79768737+iamsouvik9@users.noreply.github.com>
2023-09-10 18:32:57 +0530

    Create req.sh

---
+++ req.sh
@@ -0,0 +1,9 @@
+
+
+sudo apt install -y git
+sudo apt install -y apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
+sudo rm -rf /var/www/html
+sudo rm -rf /var/www
+sudo git clone https://github.com/iamsouvik9/CICD-Website.git /var/www/html
